8e61ea2aced8000e7d28dff4722a55a0b40f1d8d
emmanuel umania <dacha1252@gmail.com>
2019-10-26 12:32:35 -0500

    bug fixing

--- src/interfaz/Controller.java
+++ src/interfaz/Controller.java
@@ -5,6 +5,8 @@ import javax.swing.JOptionPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -12,38 +14,62 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import modelo.Buscaminas;
+import modelo.NivelNoExtisteException;
 
 public class Controller {
 	Buscaminas bs;
+	@FXML
+	private BorderPane ac;
+	
+	private GridPane grd1;
 	
 	@FXML
 	private TextField txt1;
 	
 	
 	
-	public void initialize(ActionEvent event){
+	public void easyMethod(ActionEvent event){
+		
 		try {
-		int num = Integer.parseInt(txt1.getText());
-		bs = new Buscaminas(num);
-		printMatrix();
-		} catch(Exception ex) {
-			String msg = ex.getMessage();
-			JOptionPane.showMessageDialog(null, msg);
+			bs = new Buscaminas(1);
+			printMatrix();
+		} catch (NivelNoExtisteException e) {
+			e.printStackTrace();
+		}
+	}
+	public void intermMethod(ActionEvent event){
+			
+			try {
+				bs = new Buscaminas(2);
+				printMatrix();
+			} catch (NivelNoExtisteException e) {
+				e.printStackTrace();
+			}
+		}
+	public void hardMethod(ActionEvent event){
+		
+		try {
+			bs = new Buscaminas(3);
+			printMatrix();
+		} catch (NivelNoExtisteException e) {
+			e.printStackTrace();
 		}
 	}
 
 
 
 	private void printMatrix() {
-		GridPane grd1 = new GridPane();
+			grd1= new GridPane();
 
 		for (int i = 0; i < bs.darCasillas().length; i++) {
-			for (int j = 0; j < bs.darCasillas()[i].length; j++) {
+			for (int j = 0; j < bs.darCasillas().length; j++) {
 				Button b = new Button();
 				b.setText("" + bs.darCasillas()[i][j] + "");
 				b.setPrefWidth(50);
 				grd1.add(b, i, j);
 			}
 		}
+		grd1.setAlignment(Pos.CENTER);
+		ac.setCenter(grd1);
 	}
 }
